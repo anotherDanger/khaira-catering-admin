7b34fe47400d389208f05ebe4883dc67c7711704
Andhika Danger <andhikadanger666@gmail.com>
2025-06-18 05:07:38 +0700

    svc: interface, implementations

---
+++ service/service.go
@@ -0,0 +1,18 @@
+package service
+
+import (
+	"context"
+	"khaira-admin/domain"
+	"khaira-admin/web"
+)
+
+type Service interface {
+	Login(ctx context.Context, request *domain.Admin) (*web.AdminResponse, error)
+	AddProduct(ctx context.Context, request *web.Request) (*domain.Domain, error)
+	GetProducts(ctx context.Context) ([]*domain.Domain, error)
+	DeleteProduct(ctx context.Context, id string) error
+	UpdateProduct(ctx context.Context, request *web.Request, id string) (*domain.Domain, error)
+	GetOrders(ctx context.Context) ([]*domain.Orders, error)
+	UpdateOrder(ctx context.Context, entity *domain.Orders, id string) error
+	DeleteOrder(ctx context.Context, id string) error
+}
