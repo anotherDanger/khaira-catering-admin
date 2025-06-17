f4f01314ab94973583573f23bb813a6774396892
Andhika Danger <andhikadanger666@gmail.com>
2025-06-18 05:21:29 +0700

    main: create server provider for DI

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"khaira-admin/controller"
+	"khaira-admin/middleware"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
+
+func NewServer(handler controller.Controller) *fiber.App {
+	app := fiber.New()
+	app.Use(cors.New(cors.Config{
+		AllowOrigins:     "http://localhost:3000",
+		AllowCredentials: true,
+		AllowHeaders:     "Origin, Content-type, Accept, Authorization",
+		AllowMethods:     "GET, POST, PUT, DELETE",
+	}))
+
+	app.Post("/v1/login", handler.Login)
+
+	protectedRoute := app.Group("/api")
+	protectedRoute.Use(middleware.MyMiddleware)
+	protectedRoute.Get("/v1/orders", handler.GetOrders)
+	protectedRoute.Put("/v1/orders/:id", handler.UpdateOrder)
+	protectedRoute.Delete("/v1/orders/:id", handler.DeleteOrder)
+
+	protectedRoute.Post("/v1/products", handler.AddProduct)
+	protectedRoute.Get("/v1/products", handler.GetProducts)
+	protectedRoute.Delete("/v1/products/:id", handler.DeleteProduct)
+	protectedRoute.Put("/v1/products/:id", handler.UpdateProduct)
+
+	return app
+}
+
+func main() {
+
+}
