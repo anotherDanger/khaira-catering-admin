431a0cbeea50dcbc0eeed950c3bf6491bfc74e30
Andhika Danger <andhikadanger666@gmail.com>
2025-07-02 01:14:47 +0700

    web

--- web/request.go
+++ web/request.go
@@ -7,7 +7,7 @@ import (
 type Request struct {
 	Id            string     `json:"id" validate:"required"`
 	Name          string     `json:"name" validate:"required,min=5,max=50"`
-	Description   string     `json:"description" validate:"omitempty"`
+	Description   string     `json:"description"`
 	Stock         int        `json:"stock" validate:"required,number"`
 	Price         int        `json:"price" validate:"required,number"`
 	ImageMetadata string     `json:"image_metadata" validate:"max=255"`
